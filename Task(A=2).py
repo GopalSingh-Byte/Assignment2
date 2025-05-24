20fec74c347c1cf20397bdcd059b6d9ef12a9207
GopalSingh-Byte <gopalsingh.Biotech@gmail.com>
2025-05-24 17:02:45 +0530

    Add files via upload

---
+++ Task(A=2).py
@@ -0,0 +1,9 @@
+# TAsk 1 : Check if a number is even or odd
+
+# Taking integer input from the user
+num = int(input("Enter a number:"))
+# Checking if the number is even or odd
+if num %2 ==0:
+    print(f"(num) is an even number.")
+else:
+    print(f"(num) is an odd number.")
---
+++ Task(A=2)2.py
@@ -0,0 +1,9 @@
+# Initialize sum variable
+total = 0
+
+# Loop from 1 to 50
+for i in range(1, 51):
+    total += i
+
+# Display the final sum
+print(f"The sum of numbers from 1 to 50 is: {total}")
